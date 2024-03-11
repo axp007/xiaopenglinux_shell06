aa699ba49d31e132fa5e3ab4d94582585ca3f5f8
axp007 <97888584+axp007@users.noreply.github.com>
2024-03-11 18:44:20 +0800

    Add files via upload

---
+++ shujuhuifu.sh
@@ -0,0 +1,9 @@
+#恢复全量备份
+mysql -h localhost -uroot -pxiaopeng@126.com < bakdup.sql
+#在恢复全量备份之后，要将全量备份之后的增量备份也恢复回数据库中
+mysql> show master status;
+mysql> select * from users;
+mysqlbinlog /var/lib/mysql/mysql-bin.000*** | mysql -uroot -pxiaopeng@126.com name;
+#执行命令source /root/20151010.sql 执行数据库导入命令
+mysqldump -h 127.0.0.1 -uroot -p pemass ec_order > /tmp/ec_order.sql
+Enter password:shjft99@pe#mass
