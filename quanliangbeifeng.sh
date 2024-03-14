8985f8fa3c2f9f268c17adcc1da2b7cbf57f165f
axp007 <97888584+axp007@users.noreply.github.com>
2024-03-14 08:52:51 +0800

    Update quanliangbeifeng.sh

--- quanliangbeifeng.sh
+++ quanliangbeifeng.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# ##mysql用户
+# mysql用户
 user="root"
-# ##mysql密码
+# mysql密码
 userPWD="xiaopeng@126.com"
-# ##需要定时备份的数据库列表
+# 需要定时备份的数据库列表
 dbNames=(pemass mysql)
-# ##备份数据以日期创建文件夹存放,同时删除过期备份
+# 备份数据以日期创建文件夹存放,同时删除过期备份
 DATE=`date -d "now" +%Y%m%d-%H%M%S`
 ODATE=`date -d "-7day" +%Y%m%d-%H%M%S`
 newdir=/opt/db_bak/mysql/$DATE
 olddir=/opt/db_bak/mysql/$ODATE
-# ##删除过期备份数据
+# 删除过期备份数据
 if [ -d $olddir ];
   then
     rm -rf $olddir
 fi
-# ##创建新备份文件夹
+# 创建新备份文件夹
 mkdir $newdir
-# ##对备份数据库列表的所以数据库备份
+# 对备份数据库列表的所以数据库备份
 for dbName in ${dbNames[*]}
 do
   dumpFile=$dbName-$DATE.sql.gz
